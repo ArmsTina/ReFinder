92c6b52f988e29a1a7a295c5959294cbb0fa3b19
ArmsTina <didehddud1005@gmail.com>
2025-04-04 02:05:50 -0700

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <div className="header">
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#home">Suggestly</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">
+                <FontAwesomeIcon icon={faUser} />
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <FontAwesomeIcon icon={faGear} />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
